a266be379b5475db474b06e752c35b46ecdeaac4
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-10-14 20:40:09 +0700

    fix db connection

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var mysql = require('mysql2');
 var later = require('later');
 var console = require('tracer').colorConsole();
 var Joi = require('joi');
@@ -6,31 +5,16 @@ var Joi = require('joi');
 var Mailer = require('./lib/mailer');
 var DailyReport = require('./lib/reports/daily/report');
 var configSchema = require('./lib/configSchema');
+var DBManager = require('./lib/dbmanager');
 
 var server = function (config) {
 
     var init = function () {
         var mailer = new Mailer(config);
-        var conn = mysql.createConnection(config.mysql);
+        var dbManager = new DBManager(config);
+        
+        var dailyReport = new DailyReport(dbManager, mailer);
 
-        function handleDisconnect(connection) {
-            connection.on('error', function (err) {
-                if (!err.fatal) {
-                    return;
-                }
-                if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
-                    throw err;
-                }
-                console.log('Re-connecting lost connection: ' + err.stack);
-                conn = mysql.createConnection(connection.config);
-                handleDisconnect(conn);
-                conn.connect();
-            });
-        };
-
-        handleDisconnect(conn);
-
-        var dailyReport = new DailyReport(conn, mailer);
         var timer = later.setInterval(dailyReport.run, dailyReport.schedule);
         console.log('start');
     };
